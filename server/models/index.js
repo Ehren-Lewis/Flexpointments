41e4c64512cdaabe86adea20b72415230df12695
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-10-08 17:04:37 -0500

    Added the user model and validation

---
+++ server/models/index.js
@@ -0,0 +1,51 @@
+const {Schema, model} = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const emailRegEx  = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+const validateEmail = (email) => {
+    return emailRegEx.test(email)
+}
+
+
+
+// User schema only has the information needed for signing up and logging in
+// Appointments may be a single reference to the next appointment
+// I don't think I need to add a through table
+
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [validateEmail, "Input a proper email please"]
+    },
+    password: {
+        type: String,
+        required: truie
+    }
+});
+
+userSchema.pre( "save", async function (next) {
+    if ( this.isNew || this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+
+    next();
+})
+
+userSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const Users = model("Users", userSchema);
+
+module.exports = Users;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,38 @@
+
+
+// Adding mongo and the db connection
 const mongoose = require("mongoose");
-const express = require("express");
 const db = require("./config/connection");
-const PORT = 3001
-const app = express();
+const PORT = process.env.PORT || 3001
+
+
 const path = require("path");
 
+
+// Adding the appollo stuff
+const { ApolloServer }  = requir("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+
+// Setting up the express server
+const express = require("express");
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-
-db.once('open', () => {
-
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-    });
-  });
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
